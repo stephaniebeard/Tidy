44569b6b02a457fca3732b0122e2df193e0da7dc
stephaniebeard <42685323+stephaniebeard@users.noreply.github.com>
2019-07-16 23:00:58 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 
-# You should create one script called that does the following.
+# You should create one script called run_analysis.R that does the following.
 # 1.Merges the training and the test sets to create one data set.
 # 2.Extracts only the measurements on the mean and standard deviation for each measurement.
 # 3.Uses descriptive activity names to name the activities in the data set
@@ -13,6 +13,7 @@ library(dplyr)
 
 
 # download and extract the data 
+# make sure file is in the working directory
 if( !dir.exists("UCI HAR Dataset") ) {
   if( !file.exists("UCI HAR Dataset.zip") ) {
     download.file("https://www.dropbox.com/s/lfsbhwy8elgjo15/UCI_HAR_Dataset.zip?dl=1", destfile = "UCI HAR Dataset.zip")
@@ -20,7 +21,7 @@ if( !dir.exists("UCI HAR Dataset") ) {
   unzip("UCI HAR Dataset.zip")
 }
 
-#read tables into dataframe
+#read tables into dataframes
 
 X_train <- read.table(file = "./UCI HAR Dataset/train/X_train.txt")
 X_test <- read.table(file = "./UCI HAR Dataset/test/X_test.txt")
@@ -61,8 +62,8 @@ subsetted <- subset(merged, select = subsetColumns)
 
 # 3.Uses descriptive activity names to name the activities in the data set
 
-activity_labels$V2 <- as.character(activity_labels$V2)
-subsetted$ActivityName = activity_labels[subsetted$ActivityName, 2]
+activity_labels$V2 <- as.character(activity_labels$V2) #converts to string
+subsetted$ActivityName = activity_labels[subsetted$ActivityName, 2] #replaces labels
 
 
 # 4.Appropriately labels the data set with descriptive variable names.
@@ -81,8 +82,6 @@ names(subsetted) <- gsub('std\\(\\)', ' standard deviation ', names(subsetted))
 names(subsetted) <- gsub('-X', 'in X direction' , names(subsetted))
 names(subsetted) <- gsub('-Y', 'in Y direction' , names(subsetted))
 names(subsetted) <- gsub('-Z', 'in Z direction', names(subsetted))
-
-
 
 
 # 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
